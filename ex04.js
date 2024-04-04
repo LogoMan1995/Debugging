cd2c037c272e924a1f1936e919e94d467566bb6b
LogoMan1995 <david.grigorian777@gmail.com>
2024-04-04 14:06:12 +0400

    Catch Misspelled Variable and Function Names

---
+++ ex04.js
@@ -0,0 +1,9 @@
+// 4. Исправьте две орфографические ошибки в коде, чтобы netWorkingCapital расчет работал.
+
+
+
+let receivables = 10;
+let payables = 8;
+let netWorkingCapital = receivables - payables;
+console.log(`Net working capital is: ${netWorkingCapital}`);
+
