ed062bb70009104b153df697cd314a2476cb0f44
LogoMan1995 <david.grigorian777@gmail.com>
2024-04-04 14:33:12 +0400

    Catch Use of Assignment Operator Instead of Equality Operator

---
+++ ex07.js
@@ -0,0 +1,15 @@
+// 7. Исправьте условие, чтобы программа запускала правую ветвь и соответствующее значение присваивалось result.
+
+
+
+let x = 7;
+let y = 9;
+let result = "to come";
+
+if(x == y) {
+  result = "Equal!";
+} else {
+  result = "Not equal!";
+}
+
+console.log(result);
