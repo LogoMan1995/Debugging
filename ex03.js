e760845111948535048ecfcd63a667295687ae65
LogoMan1995 <david.grigorian777@gmail.com>
2024-04-04 14:02:19 +0400

    Use typeof to Check the Type of a Variable

---
+++ ex03.js
@@ -0,0 +1,6 @@
+// 3. Добавьте два console.log() оператора для проверки typeof каждой из двух переменных seven и threeв коде.
+
+let seven = 7;
+let three = "3";
+console.log(typeof(seven));
+console.log(typeof(three));
