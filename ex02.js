09b4afaf318ec9a6f0110ec8b364f9582a710979
LogoMan1995 <david.grigorian777@gmail.com>
2024-04-04 13:49:40 +0400

    Understanding the Differences between the freeCodeCamp and Browser Console

---
+++ ex02.js
@@ -0,0 +1,16 @@
+// 2. Вы должны использовать console.log() для печати output переменной. Вам следует использовать console.clear()для очистки консоли браузера.
+
+
+
+let output = "Get this to show once in the freeCodeCamp console and not at all in the browser console";
+
+console.log(output);
+
+console.clear();
+
+
+
+
+
+
+
