7e035131274608766d5d89d4826d3a236e7f14c0
LogoMan1995 <david.grigorian777@gmail.com>
2024-04-04 14:16:58 +0400

    Catch Unclosed Parentheses, Brackets, Braces and Quotes

---
+++ ex05.js
@@ -0,0 +1,5 @@
+// 5. Исправьте две пары ошибок в коде.
+
+let myArray = [1, 2, 3];
+let arraySum = myArray.reduce((previous, current) =>  previous + current);
+console.log(`Sum of array values is: ${arraySum}`);
